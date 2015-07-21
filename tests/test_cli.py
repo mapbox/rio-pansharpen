46073032bb451955ae3e281293f0799eee4c0eca
virginiayung <virginia@mapbox.com>
2015-07-20 17:28:28 -0700

    playing with test_cli

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,10 +1,56 @@
+import click
 from click.testing import CliRunner
-
 from pansharpen.scripts.pan_cli import cli
+import re
 
 
-def test_cli_count():
-    runner = CliRunner()
-    result = runner.invoke(cli, ['3'])
-    assert result.exit_code == 0
-    assert result.output == "False\nFalse\nFalse\n"
+# test raise exception
+def test_exception():
+	runner = CliRunner()
+	result = runner.invoke(
+		cli,
+		input="'../test/fixtures/tiny_20_tiffs/LC81070352015122LGN00/LC81070352015122LGN00_B8.tif'\
+		'../test/fixtures/tiny_20_tiffs/LC81070352015122LGN00/LC81070352015122LGN00_B4.tif'\
+		'../test/fixtures/tiny_20_tiffs/LC81070352015122LGN00/LC81070352015122LGN00_B3.tif'\
+		'../test/fixtures/tiny_20_tiffs/LC81070352015122LGN00/LC81070352015122LGN00_B2.tif'\
+		'../test/expected/tiny_pan_sharp_output/tiny_20_LC81070352015122LGN00_output.tif' -c 100"
+		)
+
+	assert result.exception
+
+def test_prompts():
+	@click.command()
+	@click.option('--src', prompt=True)
+	def test(src):
+		src_bands = re.findall(r"_B(?P<num_band>\d+).tif", src)
+		click.echo(src_bands)
+
+	runner = CliRunner()
+	result = runner.invoke(
+		test,
+		input="'../test/fixtures/tiny_20_tiffs/LC81070352015122LGN00/LC81070352015122LGN00_B8.tif'"
+		+ " '../test/fixtures/tiny_20_tiffs/LC81070352015122LGN00/LC81070352015122LGN00_B4.tif'"
+		+ " '../test/fixtures/tiny_20_tiffs/LC81070352015122LGN00/LC81070352015122LGN00_B3.tif'" 
+		+ " '../test/fixtures/tiny_20_tiffs/LC81070352015122LGN00/LC81070352015122LGN00_B2.tif'"
+		+" '../test/expected/tiny_pan_sharp_output/tiny_20_LC81070352015122LGN00_output.tif'"
+		)
+	assert not result.exception
+	assert re.findall(r"'(?P<band_num>\d+)'", result.output)[0] == '8'
+
+def test_prompts2():
+	runner = CliRunner()
+	result = runner.invoke(
+		cli,
+		['tests/fixtures/tiny_20_tiffs/LC81070352015122LGN00_B8.tif',
+		'tests/fixtures/tiny_20_tiffs/LC81070352015122LGN00_B4.tif',
+		'tests/fixtures/tiny_20_tiffs/LC81070352015122LGN00_B3.tif',
+		'tests/fixtures/tiny_20_tiffs/LC81070352015122LGN00_B2.tif',
+		'tests/fixtures/tiny_20_LC81070352015122LGN00_output.tif'])
+	assert not result.exception
+
+if __name__ == '__main__':
+	test_exception()
+	test_prompts()
+	test_prompts2()
+
+
